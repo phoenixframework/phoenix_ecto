50675dfea4ebd4138ca8ce7f4a4a09b2910dbfb0
José Valim <jose.valim@plataformatec.com.br>
2015-06-22 14:05:51 +0200

    Require Ecto v0.12.0

--- mix.exs
+++ mix.exs
@@ -32,6 +32,6 @@ defmodule PhoenixEcto.Mixfile do
   defp deps do
     [{:phoenix_html, "~> 1.0", optional: true},
      {:poison, "~> 1.3", optional: true},
-     {:ecto, "~> 0.11"}]
+     {:ecto, "~> 0.12"}]
   end
 end
