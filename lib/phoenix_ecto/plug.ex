fa5b235c72fa216855ad5e68d9eae1ac1d63bcaf
José Valim <jose.valim@gmail.com>
2016-04-10 11:30:11 +0200

    Also handle Ecto.Query.CastError in plug exceptions

--- lib/phoenix_ecto/plug.ex
+++ lib/phoenix_ecto/plug.ex
@@ -2,6 +2,10 @@ defimpl Plug.Exception, for: Ecto.CastError do
   def status(_), do: 400
 end
 
+defimpl Plug.Exception, for: Ecto.Query.CastError do
+  def status(_), do: 400
+end
+
 defimpl Plug.Exception, for: Ecto.NoResultsError do
   def status(_), do: 404
 end
