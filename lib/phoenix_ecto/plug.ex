05ed4df4b5611a746edc84ff3b38138b61bd9f4d
Alvin Lindstam <alvin@kundo.se>
2017-07-18 11:57:08 +0200

    Allow Plug.Exception implementations to not be included
    This will allow users to write alternate implementations or
    treat them as unhandled exceptions.
    
    Does not change behaviour unless explicitly configured to
    do so.

--- lib/phoenix_ecto/plug.ex
+++ lib/phoenix_ecto/plug.ex
@@ -1,15 +1,16 @@
-defimpl Plug.Exception, for: Ecto.CastError do
-  def status(_), do: 400
-end
+errors = [
+  {Ecto.CastError, 400},
+  {Ecto.Query.CastError, 400},
+  {Ecto.NoResultsError, 404},
+  {Ecto.InvalidChangesetError, 422},
+]
 
-defimpl Plug.Exception, for: Ecto.Query.CastError do
-  def status(_), do: 400
-end
+excluded_exceptions = Application.get_env(:phoenix_ecto, :exclude_ecto_exceptions_from_plug, [])
 
-defimpl Plug.Exception, for: Ecto.NoResultsError do
-  def status(_), do: 404
-end
-
-defimpl Plug.Exception, for: Ecto.InvalidChangesetError do
-  def status(_), do: 422
+for {exception, status_code} <- errors do
+  unless exception in excluded_exceptions do
+    defimpl Plug.Exception, for: exception do
+      def status(_), do: unquote(status_code)
+    end
+  end
 end
