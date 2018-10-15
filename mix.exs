48b2ac3ac192458eb11c18e2d2c7430036fc4ff8
José Valim <jose.valim@plataformatec.com.br>
2018-10-15 16:12:33 +0200

    Remove prerelease dep and release v3.5.0

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule PhoenixEcto.Mixfile do
 
   defp deps do
     [{:phoenix_html, "~> 2.9", optional: true},
-     {:ecto, "~> 2.2 or ~> 3.0-rc"},
+     {:ecto, "~> 2.2 or ~> 3.0"},
      {:plug, "~> 1.0"},
      {:ex_doc, ">= 0.0.0", only: :docs}]
   end
