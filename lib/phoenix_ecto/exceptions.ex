830ec23d83345da70e1d8dcaaa45dab9f4ef1f76
José Valim <jose.valim@plataformatec.com.br>
2019-11-12 23:46:13 +0100

    Make error messages clearer

--- lib/phoenix_ecto/exceptions.ex
+++ lib/phoenix_ecto/exceptions.ex
@@ -1,13 +1,17 @@
 defmodule Phoenix.Ecto.StorageNotCreatedError do
   defexception [:repo]
 
-  def message(%__MODULE__{repo: repo}),
-    do: "The storage is not created for repo: `#{inspect(repo)}`. Try `mix ecto.create`"
+  def message(%__MODULE__{repo: repo}) do
+    "the storage is not created for repo: #{inspect(repo)}. " <>
+      "Try running `mix ecto.create` in the command line to create it"
+  end
 end
 
 defmodule Phoenix.Ecto.PendingMigrationError do
   defexception [:repo]
 
-  def message(%__MODULE__{repo: repo}),
-    do: "There are pending migrations for repo: `#{inspect(repo)}`. Try `mix ecto.migrate`"
+  def message(%__MODULE__{repo: repo}) do
+    "there are pending migrations for repo: #{inspect(repo)}. " <>
+      "Try running `mix ecto.migrate` in the command line to migrate it"
+  end
 end
