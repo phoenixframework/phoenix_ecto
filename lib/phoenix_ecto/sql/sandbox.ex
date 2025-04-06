cc839015f265471bf4ffddc045ecd2880efbdc55
Fredrik Teschke <f@ftes.de>
2025-04-06 12:04:38 +0200

    Fix docs: Preserve phoenix_ecto_sandbox assign

--- lib/phoenix_ecto/sql/sandbox.ex
+++ lib/phoenix_ecto/sql/sandbox.ex
@@ -95,12 +95,15 @@ defmodule Phoenix.Ecto.SQL.Sandbox do
 
       defmodule MyApp.LiveAcceptance do
         def on_mount(:default, _params, _session, socket) do
-          %{assigns: %{phoenix_ecto_sandbox: metadata}} =
-            assign_new(socket, :phoenix_ecto_sandbox, fn ->
-              if connected?(socket), do: get_connect_info(socket, :user_agent)
-            end)
+          socket =
+            %{assigns: %{phoenix_ecto_sandbox: metadata}} =
+              assign_new(socket, :phoenix_ecto_sandbox, fn ->
+                if connected?(socket), do: get_connect_info(socket, :user_agent)
+              end)
 
           Phoenix.Ecto.SQL.Sandbox.allow(metadata, Ecto.Adapters.SQL.Sandbox)
+
+          {:cont, socket}
         end
       end
       
