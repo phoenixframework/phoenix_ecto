8fd333d6774931cdcee3033772387baecee132d7
Dorian Karter <dkarter@users.noreply.github.com>
2025-09-12 09:46:32 -0600

    docs: update docs to reflect default is `false` now

--- lib/phoenix_ecto/check_repo_status.ex
+++ lib/phoenix_ecto/check_repo_status.ex
@@ -9,7 +9,7 @@ defmodule Phoenix.Ecto.CheckRepoStatus do
 
     * `:otp_app` - name of the application which the repos are fetched from
     * `:migration_paths` - a function that accepts a repo and returns a migration directory, or a list of migration directories, that is used to check for pending migrations
-    * `:migration_lock` - the locking strategy used by the Ecto Adapter when checking for pending migrations. Set to `false` to disable migration locks.
+    * `:migration_lock` - the locking strategy used by the Ecto Adapter when checking for pending migrations. Defaults to `false`. Set to `true` to enable migration locks.
     * `:prefix` - the prefix used to check for pending migrations.
     * `:skip_table_creation` - Ecto will not try to create the `schema_migrations` table automatically.  This is useful if you are connecting as a DB user without create permissions
   """
