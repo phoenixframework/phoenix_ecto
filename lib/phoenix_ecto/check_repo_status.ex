35fcd9eb8326643940102f9bb335c651e3e58732
José Valim <jose.valim@dashbit.co>
2025-11-07 16:09:05 +0100

    Revert "Revert "Disable migration lock by default on CheckRepoStatus (#196)""
    This reverts commit 6705443afaf84d70ab0b7a728d55762155d70c70.

--- lib/phoenix_ecto/check_repo_status.ex
+++ lib/phoenix_ecto/check_repo_status.ex
@@ -9,7 +9,7 @@ defmodule Phoenix.Ecto.CheckRepoStatus do
 
     * `:otp_app` - name of the application which the repos are fetched from
     * `:migration_paths` - a function that accepts a repo and returns a migration directory, or a list of migration directories, that is used to check for pending migrations
-    * `:migration_lock` - the locking strategy used by the Ecto Adapter when checking for pending migrations. Set to `false` to disable migration locks.
+    * `:migration_lock` - the locking strategy used by the Ecto Adapter when checking for pending migrations. Defaults to `false`. Set to `true` to enable migration locks.
     * `:prefix` - the prefix used to check for pending migrations.
     * `:skip_table_creation` - Ecto will not try to create the `schema_migrations` table automatically.  This is useful if you are connecting as a DB user without create permissions
   """
@@ -67,7 +67,11 @@ defmodule Phoenix.Ecto.CheckRepoStatus do
       end)
 
     true = is_function(migrations_fun, 3)
-    migration_opts = Keyword.take(opts, @migration_opts)
+
+    migration_opts =
+      opts
+      |> Keyword.take(@migration_opts)
+      |> Keyword.put_new(:migration_lock, false)
 
     try do
       repo
